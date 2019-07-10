3c97c9546fc8722b69cdec656ea965da350f0dee
Erica Tollefson <etollefson@cnm.edu>
2019-07-10 14:29:16 -0600

    Updated Use Case and Interaction Flow to move user's flight search to a preconditional

--- epic/use-case.php
+++ epic/use-case.php
@@ -20,16 +20,13 @@
 <h2>Use Case</h2>
 		<p>Objective: For frequent/power users of Southwest.com to easily share details of a flight itinerary with friends</p>
 		<p>Description: Francine wants to quickly search for airfares and share details of specific itineraries with friends, directly from the website</p>
-		<p>Preconditional: Francine is logged-in and has previously saved her friends' contact information in her user profile</p>
+		<p>Preconditional: Francine is logged-in to Southwest.com, has previously saved her friends' contact information in her user profile, has performed an airfare search and selected flights that match her search criteria.</p>
 		<p>Postconditional: Francine purchases tickets for a trip with friends on Southwest.com</p>
 		<br>
 
 <h2>Interaction Flow</h2>
 		<ul>
-			<li>Francine opens the Southwest.com main page, and enters criteria for an airfare search</li>
-			<li>Southwest.com returns possible flights matching search criteria</li>
-			<li>Francine reviews flight details displayed in search results and clicks on fares for each desired flight, then clicks the "Continue" button</li>
-			<li>On itinerary page, Francine selects "Share" button at top of page, next to itinerary summary</li>
+			<li>On the Southwest.com itinerary page for her selcted flights, Francine selects "Share" button at top of page, next to the itinerary summary</li>
 			<li>Southwest.com returns a pop-up showing the list of friends previously saved in Francine's profile, and optional text box for adding a personal note from Francine regarding the itinerary</li>
 			<li>Francine clicks checkboxes next to each friend's name to select an option to email, text or post to Facebook or Twitter a link to the selected flight itinerary. She optionally may type a personal note in the text box.</li>
 			<li>Friends receive messages, and click on link provided in message to view the itinerary on Southwest.com.</li>
